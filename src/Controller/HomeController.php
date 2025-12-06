c2e6ea01e933bf53229da894c1b7f5e128a7136e
Marbobley <fournier.wilf@gmail.com>
2025-12-06 15:59:48 +0100

    Add meta tag

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_home' , methods: ['GET'])]
     public function index(GetAllParkingsInterface $getAllParkings): Response
     {
         $parkings = $getAllParkings->findAll();
